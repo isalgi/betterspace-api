20f6eaee0b06ec243af9acaa88e2bd3e0730d879
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2022-11-17 21:22:59 +0700

    add drivers for users

---
+++ drivers/mysql/users/mysql.go
@@ -0,0 +1,59 @@
+package user
+
+import (
+	users "backend/businesses/users"
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+type userRepository struct {
+	conn *gorm.DB
+}
+
+func NewMySQLRepository(conn *gorm.DB) users.Repository {
+	return &userRepository{
+		conn: conn,
+	}
+}
+
+func (ur *userRepository) Register(userDomain *users.Domain) users.Domain {
+	password, _ := bcrypt.GenerateFromPassword([]byte(userDomain.Password), bcrypt.DefaultCost)
+	
+	rec := FromDomain(userDomain)
+	rec.Password = string(password)
+	
+	var user User
+	ur.conn.First(&user, "email = ?", userDomain.Email)
+
+	// handle email if email already used for an account
+	if user.ID != 0 {
+		fmt.Println("user exist. proceed to login or use another email.")
+		return users.Domain{}
+	}
+
+	result := ur.conn.Create(&rec)
+	result.Last(&rec)
+
+	return rec.ToDomain()
+}
+
+func (ur *userRepository) GetByEmail(userDomain *users.Domain) users.Domain {
+	var user User
+	ur.conn.First(&user, "email = ?", userDomain.Email)
+
+	if user.ID == 0 {
+		fmt.Println("user not found")
+		return users.Domain{}
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userDomain.Password))
+
+	if err != nil {
+		fmt.Println("password failed")
+		return users.Domain{}
+	}
+
+	return user.ToDomain()
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-faker/faker/v4 v4.0.0-beta.3 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
