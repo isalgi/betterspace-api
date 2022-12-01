f1cbbc5f6869453f90503ecdb74787d36215a95b
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2022-12-01 23:29:24 +0700

    edit response for human-readable

--- controllers/users/response/json.go
+++ controllers/users/response/json.go
@@ -1,29 +1,24 @@
 package response
 
-import (
-	"backend/businesses/users"
-	"time"
-
-	"gorm.io/gorm"
-)
+import "backend/businesses/users"
 
 type User struct {
-	ID        uint           `json:"id" gorm:"primaryKey"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at"`
-	FullName  string         `json:"full_name"`
-	Gender    string         `json:"gender"`
-	Email     string         `json:"email"`
-	Photo     string         `json:"photo" form:"photo"`
+	ID        uint   `json:"id" gorm:"primaryKey"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	DeletedAt string `json:"deleted_at"`
+	FullName  string `json:"full_name"`
+	Gender    string `json:"gender"`
+	Email     string `json:"email"`
+	Photo     string `json:"photo" form:"photo"`
 }
 
 func FromDomain(domain users.Domain) User {
 	return User{
 		ID:        domain.ID,
-		CreatedAt: domain.CreatedAt,
-		UpdatedAt: domain.UpdatedAt,
-		DeletedAt: domain.DeletedAt,
+		CreatedAt: domain.CreatedAt.Format("02-01-2006 15:04:05"),
+		UpdatedAt: domain.UpdatedAt.Format("02-01-2006 15:04:05"),
+		DeletedAt: domain.DeletedAt.Time.Format("02-01-2006 15:04:05"),
 		FullName:  domain.FullName,
 		Gender:    domain.Gender,
 		Email:     domain.Email,
