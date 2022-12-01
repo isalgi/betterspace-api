b0fe4ac48cadf2690d71360add8fcca94449e0f3
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2022-12-01 23:29:05 +0700

    add route for admins

--- app/routes/route.go
+++ app/routes/route.go
@@ -16,17 +16,27 @@ type ControllerList struct {
 func (cl *ControllerList) RouteRegister(e *echo.Echo) {
 	e.Use(cl.LoggerMiddleware)
 
+	// default
 	e.POST("/api/v1/register", cl.AuthController.Register)
 	e.POST("/api/v1/login", cl.AuthController.Login)
 	e.POST("/api/v1/refresh", cl.AuthController.Token, middleware.JWTWithConfig(cl.JWTMiddleware))
 
+	// endpoint admin
+	admin := e.Group("/api/v1/admin", middleware.JWTWithConfig(cl.JWTMiddleware))
+	admin.GET("/users", cl.AuthController.GetAll).Name = "admin-get-all-user"
+	admin.GET("/user/:id", cl.AuthController.GetByID).Name = "admin-get-user-by-id"
+	admin.DELETE("/user/:id", cl.AuthController.Delete).Name = "admin-delete-user-account"
+	admin.PUT("/user/profile-photo/:id", cl.AuthController.UpdateProfilePhoto).Name = "admin-update-user-profile-photo"
+	admin.PUT("/user/:id", cl.AuthController.UpdateProfileData).Name = "admin-update-user-profile-data"
+
+	// endpoint user
 	users := e.Group("/api/v1/users", middleware.JWTWithConfig(cl.JWTMiddleware))
-	users.GET("", cl.AuthController.GetAll).Name = "get-all-user"
 	users.GET("/:id", cl.AuthController.GetByID).Name = "get-user-by-id"
 	users.DELETE("/:id", cl.AuthController.Delete).Name = "delete-user-account"
 	users.PUT("/profile-photo/:id", cl.AuthController.UpdateProfilePhoto).Name = "update-user-profile-photo"
-	users.PUT("/:id", cl.AuthController.UpdateProfileData).Name = "update-profile-data"
+	users.PUT("/:id", cl.AuthController.UpdateProfileData).Name = "update-user-profile-data"
 
+	// endpoint logout
 	auth := e.Group("/api/v1", middleware.JWTWithConfig(cl.JWTMiddleware))
 	auth.POST("/logout", cl.AuthController.Logout).Name = "user-logout"
 }
