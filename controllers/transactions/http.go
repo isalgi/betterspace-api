52cffb67e6c4d60c8a55a5f10eccafcd6592427c
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2022-12-18 14:58:07 +0700

    fix bug when no auth
    tadi salah hapus

--- controllers/transactions/http.go
+++ controllers/transactions/http.go
@@ -27,6 +27,14 @@ func NewTransactionController(tc transactions.Usecase) *TransactionController {
 }
 
 func (t *TransactionController) GetAll(c echo.Context) error {
+	token := c.Get("user").(*jwt.Token)
+
+	isListed := middlewares.CheckToken(token.Raw)
+
+	if !isListed {
+		return ctrl.NewInfoResponse(c, http.StatusUnauthorized, "failed", "invalid token")
+	}
+	
 	TransactionsData := t.TransactionUsecase.GetAll()
 
 	Transactions := []response.Transaction{}
